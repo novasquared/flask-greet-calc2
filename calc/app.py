3df11bb8338e50acecf226b5735e573dcbf8f759
kimberly lin <kimberly.lin10@gmail.com>
2021-09-08 12:52:41 -0700

    implemented fuctionality for calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,32 @@
-# Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+@app.get("/add")
+def add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operations.add(a,b)
+    return str(result)
+
+@app.get("/sub")
+def subtract():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operations.sub(a,b)
+    return str(result)
+
+@app.get("/mult")
+def multiply():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operations.mult(a,b)
+    return str(result)
+
+@app.get("/div")
+def divide():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operations.div(a,b)
+    return str(result)
